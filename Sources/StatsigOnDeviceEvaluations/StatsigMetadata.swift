aa95210221e369c753e25a802558badf5cc91195
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2024-10-21 16:57:12 -0700

    [release] 1.6.0 - Support Background Polling (#59)
    * Bot: Updated File StatsigOnDeviceEvaluations.podspec
    
    * Bot: Updated File Sources/StatsigOnDeviceEvaluations/StatsigMetadata.swift

--- Sources/StatsigOnDeviceEvaluations/StatsigMetadata.swift
+++ Sources/StatsigOnDeviceEvaluations/StatsigMetadata.swift
@@ -5,7 +5,7 @@ class StatsigMetadata {
         StatsigMetadata()
     }()
     // SDK Info
-    let sdkVersion = "1.5.0"
+    let sdkVersion = "1.6.0"
     let sdkType = "swift-on-device-eval"
     let sessionID: String
 
